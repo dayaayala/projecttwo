d2fee015d982fb47d8c8473b25e05d2f50e69ea2
dayaayala <dayala2@vols.utk.edu>
2026-03-24 09:56:42 -0400

    idk

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Use your GitHub repo name. If the repo is "project2", base is '/project2/'
-// If your repo is "Daya-UV-Index" or something else, change it to '/Daya-UV-Index/'
+// Must match the repo name for GitHub Pages: https://<user>.github.io/<repo>/
 export default defineConfig({
   plugins: [react()],
-  base: '/project2/',
+  base: '/projecttwo/',
 })
